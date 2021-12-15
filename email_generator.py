b60d53cb604f5f7ffe539837283c397478ccf6e2
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2021-12-14 23:13:37 -0300

    Create email_generator.py

---
+++ email_generator.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import random
+import os
+import time
+
+# Dados aleatorios
+
+def email_senha():
+
+    os.system('clear')
+
+    print('''Gerador de email e senha\n''')
+
+    # caracteres = '1, 2, 3, 4, 5, 6, 7, 8, 9, 0'
+    caracteres = '_', '-', ''
+
+    caracteres_random= random.choice(caracteres)
+    final = random.randint(1,100)
+
+    nome = str(input('Digite um nome para o seu email: '))
+    sobrenome = str(input('\nDigite o seu sobrenome para complementar no email: '))
+
+    try:
+        # Montando a senha
+        lower = 'abcdefghijklmnopqrstuvwxyz'
+        upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        numbers = '0123456789'
+        symbols = '#@$%&*-=+/_'
+
+        os.system('clear') # CLEAR
+
+        # VARS_ALL
+        vars_all = lower + upper + numbers + symbols
+
+        # Tamanho da senha
+        length = int(input('Digite o tamanho da sua senha (Limite de senha - 73)): '))
+
+        if length > 73:
+            print('\nSenha muito grande tente uma menor...')
+            time.sleep(2)
+            email_senha()
+
+        # Gerando a senha
+        password = ''.join(random.sample(vars_all , length))
+        print (f'\nEmail: {nome}{caracteres_random}{sobrenome}{final}@gmail.com')
+        print(f'\nSenha: {password}')
+
+    except KeyboardInterrupt:
+        os.system('clear')
+        exit()
+
+email_senha()
